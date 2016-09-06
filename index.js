985ef4c7b8cd68df0488aabf8df1d35cf25ed922
Kris <kris@afkm.io>
2016-09-06 16:37:16 +0200

    Add files

---
+++ index.js
@@ -0,0 +1,75 @@
+var template = require('./index.html');
+
+(function () {
+    'use strict';
+
+    return angular
+        .module('kio.ui-button', [])
+        .directive('uiButton', uiButton);
+
+
+    /** @ngInject */
+    function uiButton () {
+        var directive = {
+            restrict: 'E',
+            templateUrl: template,
+            scope: true,
+            controller: UiButtonController,
+            controllerAs: 'button',
+            link: linkFunc,
+            bindToController: {
+                type: '@',
+                text: '@',
+                color: '@',
+                ref: '@',
+                state: '@'
+            }
+        };
+
+        return directive;
+
+
+        function linkFunc (scope, ele, attr, vm) {
+
+            vm.link = ele[0].querySelector("a");
+        }
+
+
+        /** @ngInject */
+        function UiButtonController () {
+
+            var vm = this;
+
+            /// @param type can be
+            /// default,
+            /// close, plus, plus-small, minus, next, top, top-small, pause, rewind, play,
+            /// back-square, close-square,
+            /// down-trans,
+            /// continue, bigstart
+            /// openclose
+
+            if (!vm.type) {
+
+                vm.type = 'default';
+            }
+
+
+            if (!vm.state) {
+
+                vm.state = 'tbd';
+            }
+
+
+            // if this is no real link, unfocus after click
+
+            vm.blurCond = function () {
+
+                if (!vm.ref) {
+
+                    vm.link.blur();
+                }
+            };
+        }
+    }
+
+})();
